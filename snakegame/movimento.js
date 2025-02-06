93e6eeace85dabf49a4c965900a769a25ead5055
Gust-Dev <dev.contato.luiz@gmail.com>
2025-02-06 19:15:20 -0300

    correção do canvas

---
+++ snakegame/movimento.js
@@ -0,0 +1,10 @@
+//Para fazer a movimentação do bloco - vou precisar fazer -->
+// Att do estado do bloco no Canva
+//Movimentação por PX com variação das Teclas 
+
+//inicio -- input do teclado - captura de input
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx 
