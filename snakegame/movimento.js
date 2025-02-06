4269cc8fd2253b30dd6f031a553552af476a8c20
Gust-Dev <dev.contato.luiz@gmail.com>
2025-02-06 20:09:34 -0300

    att snake move

--- snakegame/movimento.js
+++ snakegame/movimento.js
@@ -7,5 +7,44 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red"
-ctx.fillRect(300, 300, 20, 20)
+
+const size = 20;
+const snake = [ 
+    {x: 200, y:200},
+    {x: 220, y:200}
+  
+];  //posição da cobra no mapa
+
+let direction = "rigth"
+
+const drawSnake = () => {
+    ctx.fillStyle = "#ddd"
+    snake.forEach((position, index)=> {
+        if (index == snake.length - 1 ) {
+            ctx.fillStyle = "red"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+
+    
+}
+
+const moveSnake = () => {
+    const head = snake[snake.length - 1]
+
+    if (direction == "rigth") {
+        snake.push({x:head.x+size, y:head.y})
+        
+    }
+    snake.shift()
+}
+setInterval(()=>{
+    ctx.clearRect(0,0, 600, 600)
+    drawSnake()
+    moveSnake()
+}, 300)
+
+// A cobra é um array com posições e cada quadrado é um elemento -
+// Seguindo essa logica, entenda que para ela se movimentar remover o ultimo bloco do array
+// E o colocar na frente vai ser a logica a ser utilizada.
